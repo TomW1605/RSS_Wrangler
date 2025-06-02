997b04571fce9277c8ec3c552f85faef3c8e600d
Thomas White <TomW1605@users.noreply.github.com>
2025-06-02 21:42:23 +0800

    disable flask debug mode

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def feed(feed_path):
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=config["port"], debug=True)
+    app.run(host="0.0.0.0", port=config["port"])
